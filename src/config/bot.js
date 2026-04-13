197da051746c76b0fafd5420ba0c85f0fc518ddc
Rex801-stack <davidhannington50@gmail.com>
2026-04-13 17:31:10 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Nexus Network").
-        name: "Nexus Network",
+        name: "Playing /help | Nexus Network",
         // Activity type number (0 = Playing).
         type: 3, 
       },
